7e6bb0af512cd47e219ce4357397b1d75977bdab
James Hartt <jameshartt@gmail.com>
2014-12-17 19:17:36 +0000

    Project Creation
     Changes to be committed:
    	new file:   twosquared/twosquared.xcodeproj/project.pbxproj
    	new file:   twosquared/twosquared.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    	new file:   twosquared/twosquared/AppDelegate.swift
    	new file:   twosquared/twosquared/Base.lproj/LaunchScreen.xib
    	new file:   twosquared/twosquared/Base.lproj/Main.storyboard
    	new file:   twosquared/twosquared/Images.xcassets/AppIcon.appiconset/Contents.json
    	new file:   twosquared/twosquared/Info.plist
    	new file:   twosquared/twosquared/ViewController.swift
    	new file:   twosquared/twosquaredTests/Info.plist
    	new file:   twosquared/twosquaredTests/twosquaredTests.swift

---
+++ twosquared/twosquared/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twosquared
+//
+//  Created by James Hartt on 17/12/2014.
+//  Copyright (c) 2014 SecondT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
